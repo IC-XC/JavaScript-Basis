6d0f2bd8f7ddba4e734f5af06494551ed5a2e8ae
Per <86513555+IC-XC@users.noreply.github.com>
2021-09-05 15:43:29 +0200

    Add files via upload

---
+++ Document Object Model/console.js
@@ -0,0 +1,4 @@
+
+var eDivs = document.getElementsByTagName('div');
+var nAantal = eDivs.length;
+console.log("dit document bevat in totaal " + nAantal + " div elementen");
---
+++ Document Object Model/script.js
@@ -0,0 +1,4 @@
+
+
+const paraText = document.getElementById("p1");
+paraText.innerHTML += " en <em>chocolade</em>";
