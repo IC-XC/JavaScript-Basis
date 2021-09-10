9384ff129396c68307da709067c182e33d1a5501
Per <86513555+IC-XC@users.noreply.github.com>
2021-09-10 15:44:51 +0200

    Add files via upload

---
+++ Opdracht DOM/hyperlink.js
@@ -0,0 +1,4 @@
+var demo = document.getElementById("demo");
+var anchorElem =  demo.querySelector('a');
+anchorElem.href = 'https://www.google.com/search?q=science-fiction&tbm=isch&source=hp'; 
+anchorElem.target = '_blank';
