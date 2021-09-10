dc7c85d076e0d24c73066ddc40eb36ea531d5a19
Per <86513555+IC-XC@users.noreply.github.com>
2021-09-10 15:44:09 +0200

    Delete hyperlink.js

--- Opdracht DOM/hyperlink.js
+++
@@ -1,4 +0,0 @@
-var demo = document.getElementById("demo");
-var anchorElem =  demo.querySelector('a');
-anchorElem.href = 'https://www.google.com/search?q=science-fiction&tbm=isch&source=hp'; 
-anchorElem.target = '_blank';
