23f12c8b3a10c870cd47dbc17bb523e47711ce71
Per <86513555+IC-XC@users.noreply.github.com>
2021-09-10 15:42:09 +0200

    Add files via upload

---
+++ Getallenreeksen/script.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+ 
+  //=============variabelen=======
+  var eOutput = document.getElementById('output');
+  var eKnop = document.getElementById('deKnop');
+  var eGetal = document.getElementById('getal');
+
+  //=============event handlers===========
+  eKnop.onclick = function(){
+    var nGetal = eGetal.value;
+    if(nGetal==""||isNaN(nGetal)){
+         alert('Deze functie werkt enkel met getallen');
+    }
+    else{
+    eOutput.innerHTML = lussenMaar(parseInt(nGetal))
+    }
+};
+
+}//einde window.onload
+
+//=============functies=================
+function lussenMaar(n){
+  /* Testfunctie voor iteraties : FOR
+  @n getal, verplicht, max aantal iteraties
+  */
+  //console.log(n);
+  var sTekst = "";
+  for(var i=1; i<= n ; i++){
+       for(var j=1;j<=n;j++){
+       sTekst += i*j + " ";
+       }
+       sTekst += "<br>";
+  }
+  return sTekst;
+}
